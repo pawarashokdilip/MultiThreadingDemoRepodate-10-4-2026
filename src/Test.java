b577f56b38e31bfcbcf5f85a923c7b1b5cd7981f
ashokp <ashok.pawar@synoris.co.in>
2026-04-10 15:23:47 +0530

    comment is added

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,26 @@
-
+// Main class to run threads
 public class Test {
-public static void main(String[] args) {
-	
-	MyThread mt1= new MyThread();
-	MyThread mt2=new MyThread();
-	mt1.start();
-	mt2.start();
-}
+
+    public static void main(String[] args) {
+
+        // Creating two thread objects
+        MyThread mt1 = new MyThread();
+        MyThread mt2 = new MyThread();
+
+        // Starting threads (this will call run() internally)
+        mt1.start();
+        mt2.start();
+    }
 }
 
-class MyThread extends Thread
-{
-	public void run() {
-		System.out.println(Thread.currentThread().getName());
-	}
+// Custom Thread class extending Thread
+class MyThread extends Thread {
+
+    // Overriding run() method (thread execution logic)
+    @Override
+    public void run() {
+
+        // Printing current thread name
+        System.out.println(Thread.currentThread().getName());
+    }
 }
