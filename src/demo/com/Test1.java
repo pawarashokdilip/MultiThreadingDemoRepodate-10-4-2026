b9423f69006a6e74fada891585e2342d10e5ecb9
ashokp <ashok.pawar@synoris.co.in>
2026-04-10 18:54:45 +0530

    added comment in this code

--- src/demo/com/Test1.java
+++ src/demo/com/Test1.java
@@ -1,41 +1,62 @@
 package demo.com;
 
+// Main class
 public class Test1 {
-	public static void main(String[] args) throws InterruptedException {
-		Printer p = new Printer();
-		Mythread mt1 = new Mythread(p, "TATAmoters");
-		mt1.setName("mt1");
-		Mythread mt2 = new Mythread(p, "ZeRODA");
-		mt2.setName("mt2");
-		mt1.start();
-		mt2.start();
 
-	}
+    public static void main(String[] args) throws InterruptedException {
+
+        // Create one shared Printer object
+        Printer p = new Printer();
+
+        // Create first thread and pass printer + share name
+        Mythread mt1 = new Mythread(p, "TATAmoters");
+        mt1.setName("mt1"); // Set thread name
+
+        // Create second thread
+        Mythread mt2 = new Mythread(p, "ZeRODA");
+        mt2.setName("mt2"); // Set thread name
+
+        // Start both threads
+        mt1.start();
+        mt2.start();
+    }
 }
 
+// Custom Thread class
 class Mythread extends Thread {
-	Printer p;
-	String name;
 
-	public Mythread(Printer p, String name) {
-		super();
-		this.p = p;
-		this.name = name;
-	}
+    Printer p;   // Shared Printer object
+    String name; // Share name to print
 
-	public void run() {
-		p.printSharename(name);
-		
+    // Constructor to initialize Printer and name
+    public Mythread(Printer p, String name) {
+        super();
+        this.p = p;
+        this.name = name;
+    }
 
-	}
+    // run() method executed when thread starts
+    public void run() {
 
+        // Calling synchronized method of Printer
+        p.printSharename(name);
+    }
 }
 
+// Shared resource class
 class Printer {
-	public  synchronized void    printSharename(String name) {
-		for (int i = 0; i <=10; i++) {
-			System.out.println("share name"+ name+"Printrd by ThreadName" + Thread.currentThread().getName());
 
-		}
-	}
+    // synchronized method → only ONE thread can execute at a time
+    public synchronized void printSharename(String name) {
+
+        // Loop to print share name multiple times
+        for (int i = 0; i <= 10; i++) {
+
+            // Print share name along with thread name
+            System.out.println(
+                "Share Name: " + name +
+                " printed by Thread: " + Thread.currentThread().getName()
+            );
+        }
+    }
 }
